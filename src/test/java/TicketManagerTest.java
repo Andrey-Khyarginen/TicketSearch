f67ee16b5d98099fd6821de0a5af1739c6605ca5
Andrey Kharginen <kut13@inbox.ru>
2023-03-02 11:01:56 +0400

    add test

--- src/test/java/TicketManagerTest.java
+++ src/test/java/TicketManagerTest.java
@@ -76,4 +76,28 @@ class TicketManagerTest {
         Ticket[] actual = manager.findAll("NEW", "LA");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void testSortTickets4() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        Ticket ticket1 = new Ticket(1, "MSK", "SPB", 100, 93);
+        Ticket ticket2 = new Ticket(2, "MSK", "SPB", 200, 98);
+        Ticket ticket3 = new Ticket(3, "MSK", "UFA", 300, 186);
+        Ticket ticket4 = new Ticket(4, "MSK", "SPB", 400, 122);
+        Ticket ticket5 = new Ticket(5, "UFA", "SPB", 500, 222);
+        Ticket ticket6 = new Ticket(6, "MSK", "SPB", 600, 130);
+        Ticket ticket7 = new Ticket(7, "NEW", "LA", 700, 253);
+        Ticket ticket8 = new Ticket(8, "MSK", "SPB", 800, 99);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("UFA", "LA");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
